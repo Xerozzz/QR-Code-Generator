0d84e7f545ababa4c9f2486cb9bae0a8e124587c
Xerozzz <60227509+Xerozzz@users.noreply.github.com>
2022-08-01 23:39:30 +0800

    .

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import qrcode
 from PIL import Image
 
 app = Flask(__name__)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
